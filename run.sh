43ad799e6f3ad3d024327cc3c942044b07aaa142
jeffrey <jeffrey.p.mcateer@gmail.com>
2016-06-09 14:14:09 -0400

    moving things about

--- run.sh
+++ run.sh
@@ -62,7 +62,12 @@ rm .middle*
 
 javac *.java || exit 0
 
-java -ea -Xms6g -Xmx6g Test
+if [ $(hostname) == "cs.cpsc.umw.edu" ]; then
+  echo "Increasing Heap memory, we are on CS server"
+  java -ea -Xmsg -Xmxg Test
+else
+  java -ea -Xms6g -Xmx6g Test
+fi
 
 rm Test.java
 rm *.class
